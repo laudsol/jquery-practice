ed69b7c43b1152c6cd962a65d168e7c35ab69aeb
Sol Laudon <sollaudon@Sols-MacBook-Pro.local>
2023-06-06 10:03:32 -0500

    add div, define new dix in jquery and add sibling

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 $(document).ready(() => {
-    console.log('Im working!!!!!');
+    // grabs the element with ID starting-point
+    const startingElement = $("#starting-point");
+    // creates a new div 
+    const siblingElement = $("<div></div>");
+    // adds test tp the new div
+    siblingElement.text("This is the next part");
+    // appends the new div to startingElement
+    startingElement.after(siblingElement);
+
 });
